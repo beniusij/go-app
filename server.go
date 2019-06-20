085042223ebc3b60a33c89c058b4a2e3773beebf
Juozas Beniusis <beniusij@gmail.com>
2019-06-20 14:59:03 +0100

    Create a test and dummy main script.

--- server.go
+++ server.go
@@ -1,5 +1,9 @@
+package main
+
+import "net/http"
+
 type Handler interface {
-	ServeHTTP(ResponseWriter, *Request)
+	ServeHTTP(w http.ResponseWriter, r *http.Request)
 }
 
-func ListenAndServe(addr string, handler Handler) error
+//func ListenAndServe(addr string, handler Handler) error {}
