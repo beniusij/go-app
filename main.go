09b526aa353100009b128b12ebe1e39cadcd08a4
Juozas Beniusis <beniusij@gmail.com>
2019-07-03 21:24:52 +0100

    Finish HTTP Server chapter

--- main.go
+++ main.go
@@ -5,9 +5,26 @@ import (
 	"net/http"
 )
 
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+type InMemoryPlayerStore struct{
+	store map[string]int
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
+
 func main() {
-	handler := http.HandlerFunc(PlayerServer)
-	if err := http.ListenAndServe(":8080", handler); err != nil {
+	server := &PlayerServer{NewInMemoryPlayerStore()}
+
+	if err := http.ListenAndServe(":8080", server); err != nil {
 		log.Fatalf("could not listen on port 8080 %v", err)
 	}
 }
